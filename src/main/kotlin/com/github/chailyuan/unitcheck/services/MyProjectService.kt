d349d92a4759857f9150243f3edc39bbc9d6a8a3
GitHub Action <action@github.com>
2022-09-29 05:10:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/chailyuan/unitcheck/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chailyuan/unitcheck/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chailyuan.unitcheck.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chailyuan.unitcheck.MyBundle
 
 class MyProjectService(project: Project) {
 
